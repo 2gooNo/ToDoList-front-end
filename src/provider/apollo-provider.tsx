63668f8037c2f79d1a4edb2e71d3cc10b2c0f046
24HP0317 24HP0317 <24HP0317@Administrators-iMac.local>
2024-06-14 18:05:32 +0800

    hhaha

---
+++ src/provider/apollo-provider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const ApolloProviders = (children: any) => {
+  const client = new ApolloClient({
+    uri: " http://localhost:8080/api/graphql",
+    cache: new InMemoryCache(),
+  });
+  return (
+    <>
+      <ApolloProvider client={client}>{children}</ApolloProvider>
+    </>
+  );
+};
+
+export default ApolloProviders;
