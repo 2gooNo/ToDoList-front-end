6d2baa521cc107e4930a2ef5a6e100684a912393
2gooNo <143684446+2gooNo@users.noreply.github.com>
2024-06-17 23:27:32 +0800

    codegen and little dev

--- src/app/_components/AddTodo.tsx
+++ src/app/_components/AddTodo.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
@@ -10,10 +10,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import axios from "axios";
-import Home from "../page";
-import { TodoType } from "@/lib/types";
 import { useAddTodoMutation } from "@/generated/pages/index";
+import AddTeam from "./AddTeam";
 
 const AddTodo = () => {
   const [inputAppear, setInputAppear] = useState(false);
@@ -25,44 +23,52 @@ const AddTodo = () => {
     },
   });
 
-  const handleAdd = () => {
+  const handleAddTodo = () => {
     addTodoMutation;
   };
+  if (error) {
+    return <div>Error</div>;
+  }
+  if (loading) {
+    return <div>This is loading</div>;
+  }
 
-  return (
-    <div className="flex gap-2 max-w-[800px] m-auto mb-[40px]">
-      <Input
-        type="text"
-        placeholder="Todo Title"
-        onChange={(e) =>
-          setAddTodoData((prev) => ({ ...prev, todoTitle: e.target.value }))
-        }
-      />
-      <Button variant={"outline"} onClick={() => handleAdd()}>
-        Add
-      </Button>
-      <div>
-        <Select
-          onValueChange={(e) =>
-            setAddTodoData((prev) => ({ ...prev, groupVal: e }))
+  if (!loading) {
+    return (
+      <div className="flex gap-2 max-w-[800px] m-auto mb-[40px]">
+        <Input
+          type="text"
+          placeholder="Todo Title"
+          onChange={(e) =>
+            setAddTodoData((prev) => ({ ...prev, todoTitle: e.target.value }))
           }
-        >
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Team" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="My-1">My-1</SelectItem>
-            <SelectItem value="My-2">My-2</SelectItem>
-            <SelectItem value="My-3">My-3</SelectItem>
-          </SelectContent>
-        </Select>
+        />
+        <Button variant={"outline"} onClick={() => handleAddTodo()}>
+          Add
+        </Button>
+        <div>
+          <Select
+            onValueChange={(e) =>
+              setAddTodoData((prev) => ({ ...prev, groupVal: e }))
+            }
+          >
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Team" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="My-1">My-1</SelectItem>
+              <SelectItem value="My-2">My-2</SelectItem>
+              <SelectItem value="My-3">My-3</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <Button onClick={() => setInputAppear(!inputAppear)}>
+          {inputAppear ? "Cancel" : "Add team"}
+        </Button>
+        {inputAppear && <AddTeam />}
       </div>
-      <Button onClick={() => setInputAppear(!inputAppear)}>
-        {inputAppear ? "Cancel" : "Add team"}
-      </Button>
-      {inputAppear && <Input type="text" placeholder="Enter team name" />}
-    </div>
-  );
+    );
+  }
 };
 
 export default AddTodo;
--- src/provider/apollo-provider.tsx
+++ src/provider/apollo-provider.tsx
@@ -2,7 +2,9 @@
 
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-const ApolloProviders = ({ children }) => {
+interface Props extends React.PropsWithChildren {}
+
+const ApolloProviders = ( {children }:Props) => {
   const client = new ApolloClient({
     uri: " http://localhost:8080/api/graphql",
     cache: new InMemoryCache(),
