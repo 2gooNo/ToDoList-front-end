fdabbf945f8d42bbcb310823eb6e1e65f7dbb853
24HP0317 24HP0317 <24HP0317@Administrators-iMac.local>
2024-06-19 17:39:26 +0800

    back end endpoint setted

--- src/provider/apollo-provider.tsx
+++ src/provider/apollo-provider.tsx
@@ -6,7 +6,7 @@ interface Props extends React.PropsWithChildren {}
 
 const ApolloProviders = ({ children }: Props) => {
   const client = new ApolloClient({
-    uri: " http://localhost:8000/api/graphql",
+    uri: " https://todo-back-end-two.vercel.app/api/graphql",
     cache: new InMemoryCache(),
   });
   return (
